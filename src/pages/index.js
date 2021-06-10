6932b5212404910f01288707df8bcbe38e973582
Massimo Regaglia <m.regaglia@gmail.com>
2021-06-10 22:28:19 +0200

    Global styles and fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import * as React from "react";
+import 'normalize.css';
+import GlobalStyles from "../styles/GlobalStyles";
+import Typography from "../styles/Typography";
 
 const IndexPage = () => {
   return (
+  <>
+  <GlobalStyles/>
+  <Typography/>
     <p>Hello</p>
+    </>
   )
 }
 
